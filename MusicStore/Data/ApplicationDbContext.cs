cb53e6ce747fc075b73dae6eb8135f3b059e9696
zyd16888 <zyd475182659@163.com>
2019-05-19 15:11:45 +0800

    添加项目文件。

---
+++ MusicStore/Data/ApplicationDbContext.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using MusicStore.Models;
+
+namespace MusicStore.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Music> Musics { get; set; }
+        public DbSet<Genre> Genres { get; set; }
+        public DbSet<Singer> Singers { get; set; }
+    }
+}
